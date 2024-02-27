45eb9405ae87126c489c9648adbf682bed145d0b
dbooyah93 <davidmcruz1125@gmail.com>
2024-02-26 22:57:53 -0600

    Leverage deduping to import data to posts/id page

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,4 +1,9 @@
+import { getSortedPostsData } from "@/lib/posts"
+
 export default async function page ({ params }: { params: { postId: string }}) {
+    const posts = getSortedPostsData() // deduped!
+    const { postId } = params
+
     return (
         <div>page</div>
     )
