43c0bf25af1993e332abea719bff43ce11ffa406
dbooyah93 <davidmcruz1125@gmail.com>
2024-02-26 21:23:47 -0600

    Add Navbar and Pic to layout for quality of life / noteriety

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css'
+import Navbar from './components/Navbar'
+import MyProfilePic from './components/MyProfilePic'
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="dark:bg-slate-800">
+        <Navbar/>
+        <MyProfilePic/>
+        {children}
+      </body>
     </html>
   )
 }
