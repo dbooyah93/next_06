e4f203b0f5c2c5ff3f5976c6100ade0af6981ad5
dbooyah93 <davidmcruz1125@gmail.com>
2024-02-26 23:00:18 -0600

    Import not found for post/id page

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,8 +1,14 @@
 import { getSortedPostsData } from "@/lib/posts"
+import { notFound } from "next/navigation"
 
 export default async function page ({ params }: { params: { postId: string }}) {
     const posts = getSortedPostsData() // deduped!
     const { postId } = params
+
+    if (!posts.find( post => post.id === postId)){
+        // return is not required
+        return notFound();
+    }
 
     return (
         <div>page</div>
