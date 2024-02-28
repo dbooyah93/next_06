b510548546a3df2b7787a6e754c3071fbde9290a
dbooyah93 <davidmcruz1125@gmail.com>
2024-02-27 21:25:18 -0600

    note about the fact of the matter

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link"
 import getFormattedDate from "@/lib/getFormattedDate"
 
-
-type Props = {
+type Post = {
     post: BlogPost
 }
 
-export default function LIstItem({post} : Props) {
-    const {id,title, date} = post
-    const formattedDate = getFormattedDate('2023-03-14')
+export default function ListItem({ post }: Props) {
+    const { id, title, date  } = post
+    const formattedDate = getFormattedDate(date)
+
     return (
         <li className="mt-4 text-2xl dark:text-white/90">
-            <Link className="underline hover:text-black/70 dark:hover:text-white"
-            href={`/posts/${id}`}>{title}</Link>
+            <Link className="underline
+                hover:text-black/70
+                dark:hover:text-white"
+                href={`/post/${id}`}>
+                    {/* in this case we built this
+                    component specifcally for post */}
+                    {title}
+            </Link>
             <br />
             <p className="text-sm mt-1">{formattedDate}</p>
         </li>
